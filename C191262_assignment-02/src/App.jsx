a157f30c51d33babdbd5933921ef92c8b3878822
shahnazJitu <shahnazjitu@gmail.com>
2023-12-30 00:53:19 +0600

    assignment-2

---
+++ C191262_assignment-02/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import AddEntry from "./components/AddEntry";
+import Listing from "./components/Listing";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <AddEntry />
+      <Listing />
+    </div>
+  );
+}
+
+export default App;
